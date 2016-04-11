c7c1822fddeeb87348dc3047e0f38f863d560797
imfly <kubying@foxmail.com>
2016-04-11 17:05:40 +0800

    Add features.

--- app/locales/zh-cn/translations.js
+++ app/locales/zh-cn/translations.js
@@ -27,6 +27,31 @@ export default {
         section0: {
             title: "写作/协作就是这么简单",
             desc: "给知识明码标价，让写作简单便捷。"
+        },
+        
+        section1: {
+            title: "特点",
+            desc: "书链，是世界上第一个人人可用的去中心化软件。无需去探究什么是加密货币等高深的理论，就可以轻松使用它。",
+            slide1: {
+                title: "简单",
+                desc: "只要会打字，使用过类似word，wps等办公软件，就已经具备了操控它的全部技能。"
+            },
+            slide2: {
+                title: "安全",
+                desc: "它以加密货币——博客币(ebookcoin，简称EBC）为驱动，基于一个P2P网络，具备比特币一样安全、可信和快捷等特点。"
+            },
+            slide3: {
+                title: "兼容",
+                desc: "它可以运行在各类主流硬件设备之上，Iphone, Android, Tablet，PC等，用户可以随时随地记录所思所想。"
+            },
+            slide4: {
+                title: "创新",
+                desc: "它把加密货币封装在底层，自动追加版权保护，提供无缝交易和利益分享。它驱动创作，鼓励人人贡献智慧。强化分享，可实现多人协作!"
+            }
+        },
+        opensource: {
+            title: "Ebookchain is a fully open source product",
+            desc: "Everything we do is open source, so you can theme, extend and integrate to your heart’s content. Developers welcome."
         }
     }
 };
