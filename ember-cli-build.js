cfab52022002b91684f6a7d944e02c02e6a23150
imfly <kubying@foxmail.com>
2016-04-12 19:36:30 +0800

    Update...

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      enabled: false
+      prepend: 'ebookchain.org/'
     }
   });
 
