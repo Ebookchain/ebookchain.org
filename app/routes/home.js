8f83429309524d304543bd893215804a08e60949
imfly <kubying@foxmail.com>
2016-04-22 16:28:36 +0800

    Update styles

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return Ember.RSVP.hash({
-      features: this.store.findAll('feature'),
-      partners: this.store.findAll('partner')
-    });
-  }
+    model: function() {
+        return Ember.RSVP.hash({
+            features: this.store.findAll('feature'),
+            partners: this.store.findAll('partner')
+        });
+    }
 });
