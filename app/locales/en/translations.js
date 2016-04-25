09d5628d6c10207f870ce38f378bda9f44527f72
imfly <kubying@foxmail.com>
2016-04-25 15:04:05 +0800

    Add loading.

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,74 +1,77 @@
 export default {
-  site: {
-    title: "A new driving force for human creation!",
-    language: {
-      en: "English",
-      "en-us": "English American",
-      "zh-cn": "中文"
+    site: {
+        title: "A new driving force for human creation!",
+        language: {
+            en: "English",
+            "en-us": "English American",
+            "zh-cn": "中文"
+        },
+loading: "Loading..."
+    },
+
+    header: {
+        nav: {
+            home: "Home",
+            blog: "Blog",
+            forum: "Forum",
+            ebookcoin: "Ebookcoin",
+            faq: "FAQ",
+            document: "Document",
+            signIn: "Sign in",
+            resources: "Resources"
+        }
+    },
+
+    home: {
+        num: {
+            books: '<span id="watching-{{id}}"> {{count}} </span> books',
+            users: '<span id="watching-{{id}}"> {{count}} </span> users',
+            ebc: '<span id="watching-{{id}}"> {{count}} </span> EBOOKCOIN'
+        },
+        section0: {
+            title: "Writing / collaboration is so simple",
+            desc: "To give knowledge a price tag, make writing more simple and convenient."
+        },
+
+        section1: {
+            title: "Features",
+            desc: "Ebookchain, the first usable decentralized application in the world, no need to understand some theory what the crypto-currency is.",
+            slide1: {
+                title: "Simple",
+                desc: "As long as you can type, using a similar WPS, word and other office software, you already have the skills to operate it."
+            },
+            slide2: {
+                title: "Secure",
+                desc: "It is powered by crypto-currency ebookcoin(referred to EBC), based on a P2P network, with the characteristics of safety, reliable and fast like bitcoin."
+            },
+            slide3: {
+                title: "Compatible",
+                desc: "Working in modern and old browsers too! And designed to fit to different screen sizes as well as tablet and mobile devices."
+            },
+            slide4: {
+                title: "Innovative",
+                desc: "It wrapped a crypto-currency, automatic adding copyright protection, providing seamless transactions and benefits to share. It drives and encourages everyone to  create and contribute wisdom. Strengthens sharing, and makes a number of people collaborate together!"
+            }
+
+        },
+        opensource: {
+            title: "Ebookchain is a fully open source product",
+            desc: "Everything we do is open source, so you can theme, extend and integrate to your heart’s content. Developers welcome.",
+            developers: "Developers",
+            technique: "Ebookchain is a lightning-fast Node.js application with an Ember.js admin client and Handlebars.js themes.<br>We like JavaScript.",
+            "btcnodejs-book-link": "Check out the Ebookchain developer docs →"
+        },
+        partner: {
+            title: "Partnerships",
+            desc: "Join some of the world's most important organisations who powered by Ebookchain."
+        }
+    },
+
+    footer: {
+        ebookchain: "Ebookchain",
+        resources: "Resources",
+        social_media: "Social Media",
+        qq: "ebookchain-官方QQ群：185046161",
+        weixin: "ebookchain-官方微信群"
     }
-  },
-
-  header: {
-    nav: {
-      home: "Home",
-      blog: "Blog",
-      forum: "Forum",
-      ebookcoin: "Ebookcoin",
-      faq: "FAQ",
-      document: "Document",
-      signIn: "Sign in",
-      resources: "Resources"
-    }
-  },
-
-  home: {
-    num: {
-      books: '<span id="watching-{{id}}"> {{count}} </span> books',
-      users: '<span id="watching-{{id}}"> {{count}} </span> users',
-      ebc: '<span id="watching-{{id}}"> {{count}} </span> EBOOKCOIN'
-    },
-    section0: {
-      title: "Writing / collaboration is so simple",
-      desc: "To give knowledge a price tag, make writing more simple and convenient."
-    },
-
-    section1: {
-      title: "Features",
-      desc: "Ebookchain, the first usable decentralized application in the world, no need to understand some theory what the crypto-currency is.",
-      slide1: {
-        title: "Simple",
-        desc: "As long as you can type, using a similar WPS, word and other office software, you already have the skills to operate it."
-      },
-      slide2: {
-        title: "Secure",
-        desc: "It is powered by crypto-currency ebookcoin(referred to EBC), based on a P2P network, with the characteristics of safety, reliable and fast like bitcoin."
-      },
-      slide3: {
-        title: "Compatible",
-        desc: "Working in modern and old browsers too! And designed to fit to different screen sizes as well as tablet and mobile devices."
-      },
-      slide4: {
-        title: "Innovative",
-        desc: "It wrapped a crypto-currency, automatic adding copyright protection, providing seamless transactions and benefits to share. It drives and encourages everyone to  create and contribute wisdom. Strengthens sharing, and makes a number of people collaborate together!"
-      }
-
-    },
-    opensource: {
-      title: "Ebookchain is a fully open source product",
-      desc: "Everything we do is open source, so you can theme, extend and integrate to your heart’s content. Developers welcome.",
-      developers: "Developers",
-      technique: "Ebookchain is a lightning-fast Node.js application with an Ember.js admin client and Handlebars.js themes.<br>We like JavaScript.",
-      "btcnodejs-book-link": "Check out the Ebookchain developer docs →"
-    },
-    partner: {
-        title: "Partnerships",
-        desc: "Join some of the world's most important organisations who powered by Ebookchain."
-    }
-  },
-  
-  footer: {
-      ebookchain: "Ebookchain",
-      resources: "Resources",
-      social_media: "Social Media"
-  }
 };
