9efd9c5ea7adc1def09c4debf86e17a250006168
imfly <kubying@foxmail.com>
2016-11-20 14:24:50 +0800

    修改样式

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,3 @@ gulp.task('deploy', ['domain'], function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
-
-var s3 = require("gulp-s3");
-var fs = require("fs");
-
-gulp.task('s3', function() {
-  var aws = JSON.parse(fs.readFileSync('./aws.json'));
-  gulp.src('./dist/**')
-    .pipe(s3(aws));
-});
