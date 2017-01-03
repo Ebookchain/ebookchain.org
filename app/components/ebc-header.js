5a0404897f485c029fa664a9ba45949f3fc2fae2
Evanlai8 <lsq1989@gmail.com>
2017-01-03 19:27:21 +0800

    修改背景色

--- app/components/ebc-header.js
+++ app/components/ebc-header.js
@@ -4,10 +4,12 @@ export default Ember.Component.extend({
 
     didRender() {
 	    Ember.$(window).scroll(function () {
-	        if ( Ember.$(window).scrollTop() >= Ember.$(window).height()) {
-	            Ember.$(".navbar-fixed-top").css("background-color","rgba(3,172,203,.8)");
-	        }else{
-	        	Ember.$(".navbar-fixed-top").css("background-color","rgba(0,0,0,.1)");
+	    	if(Ember.$(window).width() >= 480){
+		        if ( Ember.$(window).scrollTop() >= Ember.$(window).height()) {
+		            Ember.$(".navbar-fixed-top").css("background-color","rgba(3,172,203,.8)");
+		        }else{
+	        		Ember.$(".navbar-fixed-top").css("background-color","rgba(0,0,0,.1)");
+	        	}
 	        }
 	    });
     }
