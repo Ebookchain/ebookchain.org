2ef9bbf90f9874b02264b42b97c692a108d8365c
Evanlai8 <lsq1989@gmail.com>
2017-01-04 14:16:44 +0800

    网页翻译和一些小的修改

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -38,8 +38,18 @@ export default {
         section0: {
             title: "Writing / collaboration is so simple",
             desc: "To give knowledge a price tag, make writing more simple and convenient.",
-            writer: "Writer",
-            reader: "Reader"
+            author: "Author",
+            reader: "Reader",
+            "author-desc": "The copyright information of article content will be written into Blockchain automatically, and make blog/ebook/self-publish and so on reality.",
+            "ebookchain-desc": "Ebookchain P2P network, building an copyright protection protocol, providing functions such as token(EBC) payment/decentralized storage.",
+            "reader-desc": "Reader can use token(EBC) purchase / share / comment and feedback reading content, as well as getting knowledge and Token reward.",
+            right: "Right<br> registration",
+            resource: "Resource acquisition",
+            collaboration: "Collaborative creation",
+            blog: "Blog<br> management",
+            publication: "Publication and distribution",
+            investigation: "Investigation and evidence collection",
+            convenience: "convenience"
         },
 
         section1: {
@@ -72,6 +82,28 @@ export default {
             sourcecode: "ebookcoin"
 
         },
+        section4: {
+            title: "Ebookchain, make your creation traceable",
+            desc: "Three layer of protecting copyright in ebookchain",
+            record: "Recording the whole period of creation",
+            "record-desc": "Through ebookchain client software, recording the whole process of the content creation ,connecting all the single time stamp into time period",
+            tracing: "Tracing the whole process",
+            "tracing-desc": "The key links in the circulation is the evidence collection for coypright protection,ebookchain will record in the process of the file share/trade and so on,make it easy to trace back to all the process until the source",
+            community: "Community member participation",
+            "community-desc": "Ebookchain based on smart contract, all the Rules of rewards and punishment though token, encouraging people report and feedback, make authorize become people’s conscious behavior, make the pirate behavior no place to hide.",
+        },
+        section8: {
+            title: " DPoS mechanism improvement in Ebookchain",
+            desc: "DPoS is the most safe, environmental friendly and efficient consensus mechanism. Combining Ebookchain’s characteristics, we come up with four improved innovation.",
+            circuit: "Circuit-Breaker",
+            "circuit-desc": "Adding dissenting vote feature. It will trigger Circuit-Breaker when the dissenting vote percentage of a node reach an certain number, and the certain delegate node will be enforced to degrade.",
+            cridit: "Credit System",
+            "cridit-desc": "Ebookchain encourage active interaction between nodes and customers, the good feedback and comments will accumulate the credit of delegate node. It will help community select good delegate nodes.",
+            scaling: "Up-Scaling",
+            "scaling-desc": "The 101 delegates is a relative rational number, we'll optimize algorithm and improve the network select function, encourage the decentralized developer/publisher build their own node to provide better service for customers.",
+            realname: "Real Name Authentication",
+            "realname-desc": "Anonymous and safety are the process of balancing. Ebookchain will provide open/transparent service, encourage delegate nodes provide real-name authentication, offer relative information and accept public supervision to get a widely recognized.",
+        },
         team: {
             title: "Our Team"
         },
@@ -85,6 +117,7 @@ export default {
         ebookchain: "Ebookchain",
         ebookcoin: "Ebookcoin",
         social_media: "Social Media",
+        qqgroup: "QQ Group : ",
         qq: "ebookchain-官方QQ群：185046161",
         weixin: "ebookchain-官方微信群"
     }
