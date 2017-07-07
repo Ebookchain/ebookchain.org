31aa426116510f207e8a3cda5219a187b70982e1
Evanlai8 <lsq1989@gmail.com>
2017-07-07 15:47:17 +0800

    增加linux/mac客户端，增加更新日志

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function() {
   this.route('features');
   this.route('get_started');
   this.route('home', {'path': '/'});
-  //this.route('team');
+  this.route('team');
   //this.route('tpc');
   this.route('download');
 });
